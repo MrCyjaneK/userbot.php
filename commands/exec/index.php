4b2b5a5478ade3b9190fb0646f20cacb482b7ab2
mrcyjanek <mrcyjanek@gmail.com>
2019-11-10 20:06:00 +0100

    Added exec command

---
+++ commands/exec/index.php
@@ -0,0 +1,35 @@
+<?php
+if ($update['patient']) {
+    return 0;
+}
+if ($update['message']['out'] == true) {
+    $arg = explode(" ",$update['message']['message'],2);
+    $arg[0] = strtolower($arg[0]);
+    $cmd = $arg[0];
+    if ($cmd == ".exec") {
+        $message = "<i>Executing...</i>";
+        $MadelineProto->messages->editMessage(
+            [
+                'peer' => $update['message']['to_id'],
+                'id' => $update['message']['id'],
+                'message' => $message,
+                'parse_mode' => 'HTML' 
+            ]
+        );
+        $exec = $arg[1];
+        $result = htmlspecialchars(shell_exec($exec));
+        $message = "<i>Command: </i> <code>".$exec."</code>\n".
+                   "<b>Result:</b>\n".
+                   "<code>$result\n</code>".
+                   "<b>Path:</b> <code>".explode(PHP_EOL,shell_exec('pwd'))[0]."</code>\n".
+                   "<b>User:</b> <code>".explode(PHP_EOL,shell_exec('pwd'))[0]."</code>";
+        $MadelineProto->messages->editMessage(
+            [
+                'peer' => $update['message']['to_id'],
+                'id' => $update['message']['id'],
+                'message' => $message,
+                'parse_mode' => 'HTML' 
+            ]
+        );
+    }
+}
