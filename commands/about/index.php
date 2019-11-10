5ebf217a36caafa678431c13dc72d02353ce94ba
mrcyjanek <mrcyjanek@gmail.com>
2019-11-10 19:10:55 +0100

    added .about command

---
+++ commands/about/index.php
@@ -0,0 +1,21 @@
+<?php
+if ($update['patient']) {
+    return 0;
+}
+if ($update['message']['out'] == true) {
+    if (strtolower($update['message']['message']) == ".about") {
+        $message = "😎<b>userbot.php</b> - flexible PHP userbot\n".
+                   "🏷️<b>Version:</b> ".file_get_contents('version.txt')."\n\n".
+                   "Created by <a href=\"http://mrcyjanek.net\">Cyjan</a>\n".
+                   'You can get a free copy on <a href="https://github.com/MrCyjaneK/userbot.php">Microsoft Github</a>'."\n".
+                   "Questions? <a href=\"http://t.me/UserbotDotPHP\">@UserbotDotPHP</a>";
+        $MadelineProto->messages->editMessage(
+            [
+                'peer' => $update['message']['to_id'],
+                'id' => $update['message']['id'],
+                'message' => $message,
+                'parse_mode' => 'HTML' 
+            ]
+        );
+    }
+}
