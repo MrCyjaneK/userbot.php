3bc26e959f4eb317ddbb3f2fe060e72415ed3858
mrcyjanek <mrcyjanek@gmail.com>
2019-11-10 19:56:14 +0100

    bugfix

--- utils/login.php
+++ utils/login.php
@@ -1,8 +1,6 @@
 <?php
 if (!file_exists('madeline.php')) {
-    logger("madeline.php is not downloaded, downloading");
     copy('https://phar.madelineproto.xyz/madeline.php', 'madeline.php');
-    logger("Downloaded madeline.php");
 }
 include 'madeline.php';
 $MadelineProto = new \danog\MadelineProto\API('../userbot.php.session');
