d7d0c84eef4607d4492603cb6691664d8c1e0986
Cyjan <mrcyjanek@gmail.com>
2019-11-10 16:58:46 +0100

    everybodygit add tagall/git add tagall/!

---
+++ commands/tagall/index.php
@@ -0,0 +1,23 @@
+<?php
+if ($update['patient']) {
+    return 0;
+}
+if ($update['message']['out'] == true) {
+    $arg = explode(" ",$update['message']['message']);
+    $cmd = $arg[0];
+    if ($cmd == 'tagall') {
+        $pwr_chat = $MadelineProto->get_pwr_chat($update['message']['to_id'],['async' => false]);
+        $reply = "";
+        foreach ($pwr_chat['participants'] as $participant) {
+            $reply .= "[ ](tg://user?id=".$participant['user']['id'].")";
+        }
+        $MadelineProto->messages->editMessage(
+            [
+                'peer' => $update['message']['to_id'],
+                'id' => $update['message']['id'],
+                'message' => $update['message']['message'].$reply,
+                'parse_mode' => 'markdown' 
+            ]
+        );
+    }
+}
