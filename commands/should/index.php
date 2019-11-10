1ceb0ddf856dc38c99fb97e92f3797bb5f780123
mrcyjanek <mrcyjanek@gmail.com>
2019-11-10 18:09:06 +0100

    Bot will help you make difficult choices

---
+++ commands/should/index.php
@@ -0,0 +1,29 @@
+<?php
+if ($update['patient']) {
+    return 0;
+}
+if ($update['message']['out'] == true) {
+    $arg = explode(" ",$update['message']['message']);
+    foreach ($arg as $key => $val) {
+        $arg[$key] = strtolower($val);
+    }
+    $cmd = $arg[0];
+    if ($cmd == '.should') {
+        if (file_exists('yn.gif')) {
+            unlink('yn.gif');
+        }
+        $answer = json_decode(file_get_contents("https://yesno.wtf/api"));
+        copy($answer->image,"yn.gif");
+        $MadelineProto->messages->sendMedia([
+            'peer' => $update['message']['to_id'],
+            'media' => [
+                '_' => 'inputMediaUploadedDocument',
+                'file' => 'yn.gif',
+                'attributes' => [
+                    ['_' => 'documentAttributeAnimated']
+                ]
+            ],
+            'message' => "Personally I think that ".$answer->answer,
+        ]);
+    }
+}
