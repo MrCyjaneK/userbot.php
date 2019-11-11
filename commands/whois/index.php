818e0ffe9c8ba99669b3ee998175e008c29e2012
mrcyjanek <mrcyjanek@gmail.com>
2019-11-11 17:17:45 +0100

    Updated, thanks to @Quiec

--- commands/whois/index.php
+++ commands/whois/index.php
@@ -9,16 +9,46 @@ if ($update['message']['out'] == true) {
         $reply = "<b>Invaid syntax</b>\nUse: <code>.whois InputPeer(username, user_id)</code>";
         if (isset($arg[1])) {
             try {
-                $user = $MadelineProto->get_info($arg[1],['async' => false])["User"];
-                $reply = "<i>".$user['first_name']." ".$user['last_name']." profile</i>\n";
-                $reply .= "<b>ID:</b> <code>".$user['id']."</code>\n";
-                if (!empty($user['username'])) {
-                    $reply .= "<b>Username:</b> <code>@".$user['username']."</code>\n";
+                $info = $MadelineProto->get_full_info($arg[1],['async' => false]);
+                $name = "";
+                logger($info,5);
+                if (empty($info["full"]["user"]["last_name"])) {
+                    $lastname = "Not available.";
+                } else {
+                    $lastname = $info["full"]["user"]["last_name"];
                 }
-                if (!empty($user['scam'])) {
-                    $reply = "\nTHIS USER IS KNOWN AS SCAMMER\n";
+                if(!empty($info["full"]["about"])){
+                    $about = $info["full"]["about"];
+                } else {
+                    $about = "Not available";
                 }
-                // TODO: Send profile photo.
+                if($info["full"]["user"]["bot"] == true){
+                    $bott = "✅";
+                    $status = "Bot";
+                } else {
+                    if(empty($info["full"]["user"]["status"]["was_online"])) {
+                        if($info["full"]["user"]["status"]["_"] == "userStatusRecently") {
+                            $status = "Recently saw";
+                        } elseif ($info["full"]["user"]["status"]["_"] == "userStatusOnline") { 
+                            $status = "Online.";}
+                        } else {
+                            $status = ''.date('d.m.Y H:i:s',$info["full"]["user"]["status"]["was_online"]).'';
+                        }
+                    $bott = "❌";
+                }
+                if(!empty($info["full"]["user"]["username"])){
+                    $usernam = '@'.$info["full"]["user"]["username"].'';
+                } else {
+                    $usernam = "Not available";
+                }
+                $reply = "<b>ID:</b> <code>".$info["full"]["user"]["id"]."</code>\n".
+                "First Name: <code>".$info["full"]["user"]["first_name"]."</code>\n".
+                "Last Name: <code>".$lastname."</code>\n".
+                "Username: ".$usernam."\n".
+                "Status: <code>".$status."</code>\n".
+                "Bio: <code>".$about."</code>\n".
+                "Bot: ".$bott."";
+                // TODO: add profile photo
             } catch (Exception $e) {
                 $reply = "Error";
                 logger($e,5);
